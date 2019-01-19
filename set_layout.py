711fccea0785eb758e85dc48205e86f1b6745804
vvzvlad <vvzvlad@gmail.com>
2019-01-19 21:48:31 +0300

    add args and ctr-c handler

--- set_layout.py
+++ set_layout.py
@@ -1,6 +1,26 @@
 import numpy as np
 import cv2
+import argparse
+import signal
+import sys
 
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="video type")
+args = vars(ap.parse_args())
+
+def signal_handler(sig, frame):
+    print('\nRelease cap..')
+    cap.release()
+    cv2.destroyAllWindows()
+    print('Exit..')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+if args.get("video", None) is None:
+	cap = cv2.VideoCapture(0)
+else:
+	cap = cv2.VideoCapture(args["video"])
 # ============================================================================
 
 FINAL_LINE_COLOR = (127, 255, 127)
@@ -11,7 +31,6 @@ N_KEYCODE = 110
 
 # ============================================================================
 
-cap = cv2.VideoCapture("rtsp://admin:admin@192.168.1.29:554/RVi/1/3")
 ret, last_frame = cap.read()
 
 font                   = cv2.FONT_HERSHEY_SIMPLEX
@@ -52,6 +71,7 @@ class PolygonDrawer(object):
     def run(self):
         # Let's create our working window and set a mouse callback to handle events
         cv2.namedWindow(self.window_name, flags=cv2.WINDOW_AUTOSIZE)
+        cv2.moveWindow(self.window_name, 20, 20)
         cv2.imshow(self.window_name, last_frame.copy())
         cv2.waitKey(1)
         cv2.setMouseCallback(self.window_name, self.on_mouse)
