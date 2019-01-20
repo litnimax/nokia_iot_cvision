5ecbd3ae8e14d968759415538df379cd40088d5d
vvzvlad <vvzvlad@gmail.com>
2019-01-21 01:14:09 +0300

    work callback version

--- http_api_server.py
+++ http_api_server.py
@@ -1,30 +1,30 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from urllib import parse
-
-def http_callback(path, body):
-  return body
 
 class http_handler(BaseHTTPRequestHandler):
+    def __init__(self, callback, *args):
+        self.callback = callback
+        BaseHTTPRequestHandler.__init__(self, *args)
+
     def do_POST(self):
         content_length = int(self.headers ['Content-Length'])
         body = self.rfile.read(content_length)
         path = self.path
-        message = http_callback(path, body)
+        message = self.callback(path, body)
         self.send_response(200)
         self.send_header('Content-Type', 'application/json; charset=utf-8')
         self.end_headers()
-        self.wfile.write(message)
+        self.wfile.write(message.encode())
+        return
 
-class server():
-  def __init__(self, port):
-    self.port = port
-    self.httpd = HTTPServer(('localhost', self.port), http_handler)
+class server:
+    def __init__(self, port, callback):
+        def handler(*args):
+            http_handler(callback, *args)
+        self.httpd = HTTPServer(('', port), handler)
 
-  def __del__(self):
-    self.httpd.server_close()
+    def start(self):
+      self.httpd.serve_forever()
 
-  def start(self):
-    try:
-        self.httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
+    def __del__(self):
+      self.httpd.server_close()
+
